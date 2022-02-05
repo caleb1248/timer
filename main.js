8db7e33c4ef16c3414a88467403ecbd1f07ca641
Caleb <classicalcaleb@gmail.com>
2022-02-05 11:43:09 -0500

    added start and stop button

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 import './style.css'
-function handlePlayButtonClick(){
-  
+function handlePlayButtonClick() {
+  this.innerHTML = '<span class="material-icons">stop</span>'
+  this.onclick = handleStopButtonClick;
 }
+function handleStopButtonClick() {
+  this.innerHTML = '<span class="material-icons">play_arrow</span>'
+  this.onclick = handlePlayButtonClick;
+}
+document.querySelector('button.toggle').onclick = handlePlayButtonClick;
